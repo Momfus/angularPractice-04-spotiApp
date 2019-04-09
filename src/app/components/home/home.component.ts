27ece910bdf94e170fcbacf34b495f9e1977ea8a
Julián Muñoz Velázquez <jmunozvelazquez@mendoza.gov.ar>
2019-04-09 17:32:19 -0300

    Hasta clase 97 agregado los pedidos de servicio a spotify usando http y map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,10 +8,14 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class HomeComponent  {
 
+  nuevasCanciones: any[] = [];
 
   constructor( private spotify: SpotifyService  ) {
 
-    this.spotify.getNewReleases();
+    this.spotify.getNewReleases()
+        .subscribe( (data: any) => {
+          this.nuevasCanciones = data;
+        })
 
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root' // Esto permite que no haga falta tener que traerlo como "provider" en app.module.ts  si lo saco hay que agregar
+  providedIn: 'root' // Esto permite que no haga falta tener que traerlo como "provider" en app.module.ts  si lo saco hay que agregarlo en provider en app.module
 })
 export class SpotifyService {
 
@@ -15,16 +16,28 @@ export class SpotifyService {
 
     const headers = new HttpHeaders( {
 
-        Authorization: 'Bearer BQDHbejvCrJ6ygL1xgsfeMrKDSTlfK3yOJxYtAvuQNIKBs7C8AE4c_hnEH1kXYFy4vPfMn0x6rb800zjUxs'
+        Authorization: 'Bearer BQA-Io4yWSiVGd3pPEhOtzEBpH8BV6dncR5Oui1XS0iztH0dzvA9ArA5dtZzRVBtX05rZ5OdMNcz1U22X5s'
 
      } );
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-          .subscribe( data => {
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+                  .pipe( map( data => {
+                      // tslint:disable-next-line: max-line-length
+                      return data['albums'].items; // Esto es válido pero tambien se puede hacer data.albums.items y en map poner ( data: any )
+                  } ) );
 
-            console.log(data);
+  }
 
-          });
+  getArtista( termino: string )  {
+
+    const headers = new HttpHeaders( {
+
+      Authorization: 'Bearer BQA-Io4yWSiVGd3pPEhOtzEBpH8BV6dncR5Oui1XS0iztH0dzvA9ArA5dtZzRVBtX05rZ5OdMNcz1U22X5s'
+
+     } );
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+                  .pipe( map( data => data['artists'].items ) ); // Si es una sola línea del return, se simplifica asi si se quiere
 
   }
 
